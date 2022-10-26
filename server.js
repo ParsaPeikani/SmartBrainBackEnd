8ad43d877ba7821c2a3bf49735954f3c295a7987
Parsa <parsapeikani05@gmail.com>
2022-10-25 23:11:09 -0700

    Testing the routing for the profile users, number of times they enter the website

--- server.js
+++ server.js
@@ -64,6 +64,21 @@ app.get('/profile/:id', (req, res) => {
     }
 })
 
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    const found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            let found = true;
+            user.entries++;
+            return res.json(user.entries);
+        } 
+    });
+    if (!found) {
+        res.status(400).json('not found');
+    }
+});
+
 app.listen(3000, ()=> {
     console.log('app is running');
-})
+});
