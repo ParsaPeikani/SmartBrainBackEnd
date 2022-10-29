8e30726f1e6fc1c8218ec0d3035ae7c444255171
Parsa <parsapeikani05@gmail.com>
2022-10-29 10:48:46 -0700

    Making Signin's own component

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const knex = require('knex');
 const { response } = require('express');
 
 const register = require('./controllers/register');
+const signin = require('./controllers/signin');
 
 const db = knex({
     client: 'pg',
@@ -27,24 +28,7 @@ app.get('/', (req, res) => {
     res.send(database.users);
 })
 
-app.post('/signin', (req, res) => {
-    db.select('email', 'hash').from('login')
-    .where('email', '=', req.body.email)
-        .then(data => {
-            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-            if (isValid) {
-                return db.select('*').from('users')
-                .where('email', '=', req.body.email)
-                .then(user => {
-                    res.json(user[0])
-                })
-                .catch(err => res.status(400).json('unable to get user'))
-            } else {
-                res.status(400).json('Wrong Credentials')
-            }
-        })
-        .catch(err => res.status(400).json('wrong credentials'))
-})
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
 
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt, saltRounds) })
 
