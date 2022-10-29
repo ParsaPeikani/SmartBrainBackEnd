e331ccbfe15ba713f4e7c51fdad392f186e005bb
Parsa <parsapeikani05@gmail.com>
2022-10-28 22:36:17 -0700

    Updated the register route so that when someone registers, his information gets saved in the login table and updates the user table of the database

--- server.js
+++ server.js
@@ -56,16 +56,32 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
-    db('users')
-        .returning('*')
-        .insert({
-            email: email,
-            name: name,
-            joined: new Date()
-        })
-        .then(user => {
-            res.json(user[0]);
-        })
+
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hash = bcrypt.hashSync(password, salt);
+        db.transaction(trx => {
+            trx.insert({
+                hash: hash,
+                email: email
+            })
+            .into('login')
+            .returning('email')
+            .then(loginEmail => {
+                return trx('users')
+                    .returning('*')
+                    .insert({
+                        email: loginEmail[0].email,
+                        name: name,
+                        joined: new Date()
+                    })
+                    .then(user => {
+                        res.json(user[0]);
+                    })
+            })
+            .then(trx.commit)
+            .catch(trx.rollback)
+    })
+        
         .catch(err => res.status(400).json('unable to register'))
 })
 
